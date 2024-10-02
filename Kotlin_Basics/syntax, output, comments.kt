26c0f80b5cc1b22c9b2dcd627f0de1656306368b
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-02 21:22:48 +0600

    Create syntax, output, comments.kt

---
+++ Kotlin_Basics/syntax, output, comments.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("Hello, Sifat!")
+    println("I see, you started learning Kotlin. Good!")
+    println(3 + 3)
+
+    print("Hello, ") // will not create newline!
+    print("Folks. How are you doing?")
+    /*
+    * Multiline comments
+    * */
+}
