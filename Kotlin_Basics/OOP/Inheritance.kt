a3b2c48b2b9453bac26dba685cfe080fc7cb99f9
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-04 20:02:16 +0600

    Update Inheritance.kt

--- Kotlin_Basics/OOP/Inheritance.kt
+++ Kotlin_Basics/OOP/Inheritance.kt
@@ -1,1 +1,19 @@
+// SuperClass
+// open = Use the open keyword in front of the superclass/parent,
+// to make this the class other classes should inherit properties and
+// functions from.
+open class MyParentClass {
+    val x = 5
+}
 
+// SubClass
+class MyChildClass: MyParentClass() {
+    fun myFun() {
+        println(x)
+    }
+}
+
+fun main() {
+    val myObj = MyChildClass()
+    myObj.myFun()
+}
