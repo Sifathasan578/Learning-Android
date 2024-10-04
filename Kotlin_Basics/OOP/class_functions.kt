20400a4d888f726a25e075b14faa00a58979bf29
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-04 19:54:50 +0600

    Update class_functions.kt

--- Kotlin_Basics/OOP/class_functions.kt
+++ Kotlin_Basics/OOP/class_functions.kt
@@ -1,1 +1,17 @@
+class Car(var brand: String, var model: String, var year: Int) {
+    // function inside a class/ method
+    fun drive() {
+        println("Wroom!")
+    }
 
+    fun speed(maxSpeed: Int) {
+        println("Max speed is: $maxSpeed")
+    }
+}
+
+fun main() {
+    val c1 = Car("Ford", "Mustang", 1969)
+    println(c1.model)
+    c1.drive()
+    c1.speed(200)
+}
