a179550048f529a680f0f307037b6c0a008bee74
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-04 14:32:12 +0600

    Create Operators.kt

---
+++ Kotlin_Basics/Operators.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var sum1 = 100 + 50       // 150 (100 + 50)
+    var sum2 = sum1 + 250     // 400 (150 + 250)
+    var sum3 = sum2 + sum2    // 800 (400 + 400)
+
+    // Operator: +
+    // Operand: 50, 100
+
+    // Arithmatic Operators: +, -, *, /, %, ++, --
+    // Assignment Operators: =, +=, -=, /=, *=, %=
+    // Comparison Operators: <, >, < =, > =, ==, ! =
+    // Logical Operators: &&, ||, !
+}
