1d938a99506d648a54b078dd7ae747827e828ea0
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-04 17:23:47 +0600

    Update booleans.kt

--- Kotlin_Basics/booleans.kt
+++ Kotlin_Basics/booleans.kt
@@ -1,1 +1,15 @@
-
+fun main() {
+    var isKotlinFun: Boolean = true
+    if(isKotlinFun) {
+        println("Yes Kotlin is Fun!")
+    }
+    else {
+        println("No Kotlin is not Fun!")
+    }
+    var x = 10
+    var y = 9
+    if(x > y) println("True")
+    else println("False")
+    println(x > y)
+    println(x == 9)
+}
