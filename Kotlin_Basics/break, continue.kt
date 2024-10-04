346c331480a4befd9a6eb3121b36132d344274c2
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-04 18:27:51 +0600

    Create break, continue.kt

---
+++ Kotlin_Basics/break, continue.kt
@@ -0,0 +1,20 @@
+fun main() {
+//    var i = 0
+//    while (i < 10) {
+//        println(i)
+//        i++
+//        if (i == 4) {
+//            break
+//        }
+//    }
+
+    var i = 0
+    while (i < 10) {
+        if (i == 4) {
+            i++
+            continue
+        }
+        println(i)
+        i++
+    }
+}
