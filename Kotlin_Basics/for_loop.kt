1540294c81d0035eddf35fc9bfb806bad0003b05
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-04 18:45:18 +0600

    Create for_loop.kt

---
+++ Kotlin_Basics/for_loop.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
+    for (x in cars) {
+        println(x)
+    }
+    val nums = arrayOf(1, 5, 10, 15, 20)
+    for (x in nums) {
+        println(x)
+    }
+    // there is no traditional for loop in Kotlin.
+}
