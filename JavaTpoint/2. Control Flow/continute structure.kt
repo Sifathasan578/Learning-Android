e2801e8a0d2ed503ca166bc3a8361a5fe3207c9f
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-07 02:05:25 +0600

    Create continute structure.kt

---
+++ JavaTpoint/2. Control Flow/continute structure.kt
@@ -0,0 +1,20 @@
+fun main() {
+    for (i in 1..3) {
+        println("i = $i")
+        if (i == 2) {
+            continue
+        }
+        println("this is below if")
+    }
+
+//    Kotlin Labeled continue Expression
+    labelname@ for (i in 1..3) {
+        for (j in 1..3) {
+            println("i = $i and j = $j")
+            if (i == 2) {
+                continue@labelname
+            }
+            println("this is below if")
+        }
+    }
+}
