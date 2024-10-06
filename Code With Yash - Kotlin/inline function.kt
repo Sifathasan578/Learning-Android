ebf10eddddc0af78e568d629090c4bf2beaebd1f
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-06 18:53:46 +0600

    Update inline function.kt

--- Code With Yash - Kotlin/inline function.kt
+++ Code With Yash - Kotlin/inline function.kt
@@ -1,1 +1,14 @@
+// An inline function is declare with a keyword 'inline'
+// The use of inline function enhances the performance of higher order function
+// The inline function tells the compiler to copy parameters and functions to the call site
 
+fun main() {
+    inlineFunction { println("Calling inline function") }
+}
+
+inline fun inlineFunction (fn: () -> Unit) {
+    fn()
+    println("Code inside Inline Function!")
+}
+
+// To be continued!
