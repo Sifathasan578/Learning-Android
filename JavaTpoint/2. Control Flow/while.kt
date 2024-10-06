2f67fc803831767bd0d9ea34ca5f9636abc3f5ba
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-07 02:03:55 +0600

    Create while.kt

---
+++ JavaTpoint/2. Control Flow/while.kt
@@ -0,0 +1,30 @@
+fun main() {
+    fun main(args: Array<String>){
+        var i = 1
+        while (i<=5){
+            println(i)
+            i++
+        }
+    }
+
+//    while (true){
+//        println("infinite loop")
+//    }
+}
+
+// # Do while Loop
+fun main() {
+    var i = 1
+    do {    // executes once
+        println(i)
+        i++
+    }
+    while (i<=5);
+
+    i = 6
+    do {
+        println(i)
+        i++
+    }
+    while (i<=5);
+}
