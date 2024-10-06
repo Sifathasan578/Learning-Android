12cb81340f3324aedea25e0d2ca24ba89f0627a1
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-07 01:23:56 +0600

    Create type conversion.kt

---
+++ JavaTpoint/type conversion.kt
@@ -0,0 +1,19 @@
+fun main() {
+//    var value1 = 10
+//   val value2: Long = value1  //Compile error, type mismatch
+
+    var value1 = 10
+    val value2: Long = value1.toLong()
+
+//    toByte()
+//    toShort()
+//    toInt()
+//    toLong()
+//    toFloat()
+//    toDouble()
+//    toChar()
+
+    var value11: Long = 200
+    val value22: Int = value11.toInt()
+    println(value22)
+}
