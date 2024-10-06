5a99bfa19c503e80124cb1d934b5f2dd80abb97e
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-07 02:07:28 +0600

    Create higher order function.kt

---
+++ JavaTpoint/3. Function/higher order function.kt
@@ -0,0 +1,16 @@
+
+fun myFun(org: String, portal: String, fn: (String,String) -> String): Unit {
+    val result = fn(org, portal)
+    println(result)
+}
+
+fun main(args: Array<String>){
+    val fn:(String,String)->String={org,portal->"$org develop $portal"}
+    myFun("sssit.org", "javatpoint.com", fn)
+}
+
+//val sum: (Int, Int) -> Int = {a, b -> a + b}
+//fun myFun(org: String, portal: String, fn: (String, String) -> String): Unit {
+//    val result = fn(org,portal)
+//    println(result)
+//}
