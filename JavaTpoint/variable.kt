01f18acf0a33503ac76df97fef11904aa164ecc0
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-07 01:22:49 +0600

    Create variable.kt

---
+++ JavaTpoint/variable.kt
@@ -0,0 +1,21 @@
+fun main() {
+    println("Hello World")
+
+    var language ="Java"
+    val salary = 30000
+
+    var languagee: String ="Java"
+    val salaryy: Int = 30000
+
+    var languageee: String
+    languageee = "Java"
+
+    val salaryyy: Int
+    salaryyy = 30000
+
+    var salary1 = 30000
+    salary1 = 40000 //execute
+
+    val language1 = "Java"
+    //language1 = "Kotlin" //Error  
+}
