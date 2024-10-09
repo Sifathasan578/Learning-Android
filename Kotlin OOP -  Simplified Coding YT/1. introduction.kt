b5248d037f4c2e075d94cecd2f196684d070230e
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-10 00:44:52 +0600

    Create 1. introduction.kt

---
+++ Kotlin OOP -  Simplified Coding YT/1. introduction.kt
@@ -0,0 +1,13 @@
+/*
+OOP: A programming paradigm that uses the concepts of classes and objects
+
+Class: Blueprint / Template from where objects are created
+Object: An instance of a class
+
+Four main principles of OOP
+Encapsulation: Restricting data so that it can not be accessed by other class.
+Inheritance: We can use the properties, functions of a class in another class. (Help with code reusability)
+Abstraction: Hiding complex functionalities from user and show simple stuff
+Polymorphism: (In bangla: Onek rup taar. XD) Reusing same function to achieve different behavior
+
+*/
