a2dd497182d0c5d349e0ae594d571349c9af5f5a
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-10-13 05:58:00 +0600

    Update 9. Object Expression and Anonymous Inner Class.kt

--- Kotlin OOP -  Simplified Coding YT/9. Object Expression and Anonymous Inner Class.kt
+++ Kotlin OOP -  Simplified Coding YT/9. Object Expression and Anonymous Inner Class.kt
@@ -1,1 +1,67 @@
+/*class App: DownloadListener {
+    override fun onDownloadStarted() {
+        println("Download Started...")
+    }
 
+    override fun onDownloadCompleted(file: String) {
+        println("$file Downloaded.")
+    }
+
+    override fun onProgressUpdate(progress: Int) {
+        println("$progress% Downloaded")
+    }
+}*/
+
+fun main() {
+    /*val objectApp: App =  App()
+    val objectDownloader: Downloader = Downloader()
+
+    objectDownloader.downloadListener = objectApp
+    objectDownloader.downloadFile("FileA.mkv")*/
+
+    // Bujhci but still has confusion I guess!
+
+    // We can simplyfy this by using object expressions or anonymous inner classes
+    // Instead of creating class we can pass object expression
+
+    val objectDownloader: Downloader = Downloader()
+
+    // Object Expression / Anonymous Inner Class
+    objectDownloader.downloadListener = object : DownloadListener {
+        override fun onDownloadStarted() {
+            println("Download Started...")
+        }
+
+        override fun onDownloadCompleted(file: String) {
+            println("$file Downloaded.")
+        }
+
+        override fun onProgressUpdate(progress: Int) {
+            println("$progress% Downloaded")
+        }
+    }
+    objectDownloader.downloadFile("FileA.mkv")
+}
+
+interface DownloadListener {
+    fun onDownloadStarted() // calls each time value not null
+
+    fun onDownloadCompleted(file: String) // after loop ended
+
+    fun onProgressUpdate(progress: Int) // give updates by showing percentages: 10, 20, 30, ...
+}
+
+class Downloader {
+    var downloadListener: DownloadListener? = null
+
+    fun downloadFile(file: String) {
+        downloadListener?.onDownloadStarted() // when not null it calls
+
+        // fake the file download
+        for(i in 1..10) {
+            Thread.sleep(500)
+            downloadListener?.onProgressUpdate(i * 10)
+        }
+        downloadListener?.onDownloadCompleted(file)
+    }
+}
